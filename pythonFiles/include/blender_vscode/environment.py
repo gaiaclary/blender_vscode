d4677c5fb66fb75ff36b415d8f7abc8ded0f3d3e
Gaia Clary <gaia.clary@machinimatrix.org>
2021-05-14 13:53:42 +0200

    fix: plugin can not install python modules in Blender < 2.90

--- pythonFiles/include/blender_vscode/environment.py
+++ pythonFiles/include/blender_vscode/environment.py
@@ -3,7 +3,14 @@ import sys
 import addon_utils
 from pathlib import Path
 
-python_path = Path(sys.executable)
+def get_python_path():
+    if bpy.app.version < (2,9,0):
+        python_path = bpy.app.binary_path_python
+    else:
+        python_path = sys.executable
+    return Path(python_path)
+
+python_path = get_python_path()
 blender_path = Path(bpy.app.binary_path)
 blender_directory = blender_path.parent
 use_own_python = blender_directory in python_path.parents
